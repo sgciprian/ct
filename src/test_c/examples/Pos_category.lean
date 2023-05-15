8775b08bc146153b45c7bcb4c9e3327c840811ee
Ciprian Stanciu <g.c.stanciu@student.tudelft.nl>
2023-05-15 15:50:42 +0200

    moved f to class definition for poset

--- src/test_c/examples/Pos_category.lean
+++ src/test_c/examples/Pos_category.lean
@@ -6,12 +6,13 @@ open ct
 -- category of posets
 instance Pos : category :=
 {
-  obj := Σ α, ct.preorder α,
-  hom := λ X Y, X.1 ≤ Y.1,
-  id  := λ X, ct.poset_id X.2,
-  compose :=
+  obj := Σ α, poset α,
+  hom := λ X Y, Σ (f : X.1 → Y.1), ct.poset_morph X.2 Y.2 f,
+  id  := λ X, ⟨id, ct.poset_id X.2⟩,
+  compose := λ {X Y Z} g f, ⟨g.1 ∘ f.1, ct.poset_compose g.2 f.2⟩,
+  left_id :=
     begin
       intros,
-
+      
     end,
 }
