6910c170cbd030c2e6d9a4a81d0405408e56dcee
Ciprian Stanciu <g.c.stanciu@student.tudelft.nl>
2023-05-16 15:03:26 +0200

    small cleanup

--- src/test_c/examples/Pos_category.lean
+++ src/test_c/examples/Pos_category.lean
@@ -1,31 +1,22 @@
 import test_c.theory.category
 import test_c.theory.poset
+import data.subtype
 open ct
-
-def pos_hom (X Y : poset):= {f : X.memb → Y.memb // ∀ x₁ x₂ : X.memb, X.le x₁ x₂ → Y.le (f x₁) (f x₂)}
-
---def pos_uniq_hom : ∀ (X Y : poset), 
 
 -- Pos
 -- category of posets
 instance Pos : category :=
 {
   obj := poset,
-  hom := λ X Y, pos_hom X Y,
-  id  := λ X, ⟨λx, x, begin
-    intros x₁ x₂ h,
-    exact h,
-  end⟩,
-  compose := λ {X Y Z} g f, ⟨λx, g.1 (f.1 x), 
-  begin
-    intros x₁ x₂ h,
-    cases f with ff fp,
-    cases g with gf gp,
-    sorry,
-  end⟩,
-  left_id :=
+  hom := λ X Y, {f : X.memb → Y.memb // ∀ x₁ x₂, X.le x₁ x₂ → Y.le (f x₁) (f x₂)},
+  id  := λ X, ⟨id, begin
+      intros x₁ x₂ h,
+      exact h,
+    end⟩,
+  compose := λ {X Y Z} g f, ⟨g.val ∘ f.val, 
     begin
-      intros,
-      simp,
-    end,
+      intros x₁ x₂ h,
+      cases f with ff fp,
+      cases g with gf gp,
+    end⟩,
 }
