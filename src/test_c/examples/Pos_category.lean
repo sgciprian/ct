3a918308ac47582e20dc42b847845a1e5b2bc1e3
Ciprian Stanciu <g.c.stanciu@student.tudelft.nl>
2023-05-16 16:31:29 +0200

    removed unnecessary stuff

--- src/test_c/examples/Pos_category.lean
+++ src/test_c/examples/Pos_category.lean
@@ -8,21 +8,21 @@ open ct
 instance Pos : category :=
 {
   obj := Σ α, poset α,
-  hom := λ X Y, {f : X.1 → Y.1 // ∀ (x y : X.1) [poset X.1], X.2.le x y → Y.2.le (f x) (f y)},
+  hom := λ X Y, {f : X.1 → Y.1 // ∀ (x y : X.1), X.2.le x y → Y.2.le (f x) (f y)},
   id  := λ X, ⟨id, begin
-      intros x y h₁ h₂,
+      intros x y h₁,
       simp,
-      exact h₂,
+      exact h₁,
     end⟩,
   compose := λ X Y Z g f, ⟨g.1 ∘ f.1,
     begin
-      intros x y h₁ h₂,
+      intros x y h₁,
       simp,
       have fp := f.property,
       have gp := g.property,
-      apply @gp (f.val x) (f.val y) Y.2,
-      apply @fp x y X.2,
-      exact h₂,
+      apply gp (f.val x) (f.val y),
+      apply fp x y,
+      exact h₁,
     end⟩,
   left_id :=
     begin
