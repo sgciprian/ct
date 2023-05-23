6f8535b023e6c6508b5f65f509aa400d0146f25c
Markus Oarav <M.Orav@student.tudelft.nl>
2023-05-23 16:50:17 +0200

    Fixed initiality and terminality

--- src/universal_properties.lean
+++ src/universal_properties.lean
@@ -3,13 +3,13 @@ import .category
 namespace category_theory
 
 def isInitial (C : category) (A : C.C₀) : Prop :=
-  ∀ (B : C.C₀), ∃ (f g : C.hom A B), f = g
+  ∀ (B : C.C₀) (f g : C.hom A B), f = g
 
 structure initialObject (C : category) (A : C.C₀) :=
   (property : isInitial C A)
 
 def isTerminal (C : category) (B : C.C₀) : Prop :=
-  ∀ (A : C.C₀), ∃ (f g : C.hom A B), f = g
+  ∀ (A : C.C₀) (f g : C.hom A B), f = g
 
 structure terminalObject (C : category) (B : C.C₀) :=
   (property : isTerminal C B)
