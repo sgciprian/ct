e3057c6a64a4dc933f66896cc20fbb5c84c07d7d
Ciprian Stanciu <g.c.stanciu@student.tudelft.nl>
2023-05-29 17:22:55 +0200

    added homset to unit-counit adjunction conversion

--- src/adjunction_hom.lean
+++ src/adjunction_hom.lean
@@ -14,20 +14,13 @@ namespace category_theory
 
 -- The naturality conditions are defined for all pairs ⟨c, d⟩ of objects and
 -- arbitrary morphism dₕ in 𝒟(L c, d).
-
--- Naturality in c means this diagram commutes for all morphisms h : 𝒞(c', c):
---              φ
---   𝒟(L c,  d) → 𝒞(c,  R d)
---
---   (∘L h) ↓           ↓ (∘h)
---
---   𝒟(L c', d) → 𝒞(c', R d)
---              φ
-
-structure adjunction {C D : category} (L : functor C D) (R : functor D C) (φ : Π {c : C} {d : D}, (D.hom (L c) d) → (C.hom c (R d))) :=
+structure adjunction {C D : category} (L : functor C D) (R : functor D C) :=
+(φ : Π {c : C} {d : D}, (D.hom (L c) d) → (C.hom c (R d)))
 
 -- Homset mapping should be isomorphic.
-(bijectivity : ∀ (c : C) (d : D), function.bijective (@φ c d))
+(φr : Π {c : C} {d : D}, (C.hom c (R d)) → (D.hom (L c) d))
+(sect : ∀ {c : C} {d : D} (h : C.hom c (R d)), (φ ∘ φr) h = h)
+(retr : ∀ {c : C} {d : D} (k : D.hom (L c) d), (φr ∘ φ) k = k)
 
 -- Naturality in c means this diagram commutes for all morphisms h : 𝒞(c', c):
 --              φ
@@ -38,17 +31,43 @@ structure adjunction {C D : category} (L : functor C D) (R : functor D C) (φ : 
 --   𝒟(L c', d) → 𝒞(c', R d)
 --              φ
 (naturality_c : ∀ (c : C) (d : D) (dₕ : D.hom (L c) d),
-              ∀ {c' : C} (h : C.hom c' c), C.compose (φ dₕ) h = φ (D.compose dₕ (functor_map L h)))
-              
+              ∀ {c' : C} (h : C.hom c' c), C.compose (φ dₕ) h = φ (D.compose dₕ (L.map_hom h)))
+
 -- While naturality in d means this diagram commutes for all morphisms k : 𝒟(d, d'):
 --              φ
 --   𝒟(L c,  d) → 𝒞(c,  R d)
 --
---   (∘k) ↓           ↓ (∘R k)
+--     k ↓           ↓ R k
 --
 --   𝒟(L c, d') → 𝒞(c, R d')
 --              φ
 (naturality_d : ∀ (c : C) (d : D) (dₕ : D.hom (L c) d),
-              ∀ {d' : D} (k : D.hom d d'), C.compose (functor_map R k) (φ dₕ) = φ (D.compose k dₕ))
+              ∀ {d' : D} (k : D.hom d d'), C.compose (R.map_hom k) (φ dₕ) = φ (D.compose k dₕ))
+
+-- Since the mapping is isomorphic, we also have duals for the naturality properties, where
+-- the arrow corresponding to φ is reversed.
+--
+-- ∀ h : 𝒞(c', c):
+--              φr
+--   𝒞(c,  R d) → 𝒟(L c,  d)
+--
+--   (∘h) ↓            ↓ (∘L h)
+--
+--   𝒞(c', R d) → 𝒟(L c', d)
+--              φr
+(naturality_cr : ∀ (c : C) (d : D) (cₕ : C.hom c (R d)),
+              ∀ {c' : C} (h : C.hom c' c), D.compose (φr cₕ) (L.map_hom h) = φr (C.compose cₕ h))
+
+--
+-- ∀ k : 𝒟(d, d'):
+--              φr
+--   𝒞(c,  R d) → 𝒟(L c,  d)
+--
+--   R k ↓            ↓ k
+--
+--   𝒞(c, R d') → 𝒟(L c, d')
+--              φr
+(naturality_dr : ∀ (c : C) (d : D) (cₕ : C.hom c (R d)),
+              ∀ {d' : D} (k : D.hom d d'), D.compose k (φr cₕ) = φr (C.compose (R.map_hom k) cₕ))
 
 end category_theory
--- src/adjunction_unit.lean
+++ src/adjunction_unit.lean
@@ -10,7 +10,7 @@ namespace category_theory
 
 -- 𝒞 and 𝒟 are two categories with L : 𝒞 → 𝒟 and R : 𝒟 → 𝒞 functors.
 -- Then L and R are a pair of adjoint functors (L left adjoint, R right adjoint)
--- L ⊢ R, if we have natural transformations η, ε that fulfill the triangle identities
+-- L ⊣ R, if we have natural transformations η, ε that fulfill the triangle identities
 -- and are defined as such:
 --          η : Id D → R ⬝ L
 --          ε : L ⬝ R → Id C
@@ -27,10 +27,68 @@ namespace category_theory
 -- identity of D:
 -- for any d ∈ D₀, η (R d) maps R d to R (L (R d)),
 --             and R (ε d) maps R (L (R d)) to R d.
-
-structure adjunction_unit {C D : category} (L : functor C D) (R : functor D C)
-(η : natural_transformation (𝟙 C) (R⬝L)) (ε : natural_transformation (L⬝R) (𝟙 D)) :=
+structure adjunction_unit {C D : category} (L : functor C D) (R : functor D C) :=
+(η : natural_transformation (𝟙 C) (R⬝L))
+(ε : natural_transformation (L⬝R) (𝟙 D))
 (id_L : ∀ (c : C), D.compose (ε.α (L c)) (L.map_hom (η.α c)) = D.id (L.map_obj c))
 (id_R : ∀ (d : D), C.compose (R.map_hom (ε.α d)) (η.α (R d)) = C.id (R.map_obj d))
+
+-- Convert an adjunction defined via homset bijection to unit-counit adjunction
+def adjunction_hom_to_unit {C D} {L : functor C D} {R} (a : adjunction L R)
+: adjunction_unit L R :=
+{
+  -- the unit η can be constructing by using φ c on the identity morphism for L c,
+  -- mapping 𝒟(L c, L c) to 𝒞(c, R (L c)), which is exactly a mapping made by a 
+  -- natural transformation from Id C to R⬝L.
+  η := {
+    -- we construct the map as explained above
+    α := λ c, by exact a.φ (D.id (L c)),
+    -- and prove that it preserves the naturality condition
+    naturality_condition := 
+      begin
+        intros c' c h,
+        erw a.naturality_d,            -- use naturality in 𝒟
+        erw D.left_id,                 -- eliminate id
+        rw ← D.right_id (L.map_hom h), -- introduce id on other side
+        erw a.naturality_c,            -- use naturality in 𝒞
+        refl,
+      end,
+  },
+  -- the counit ε can be constructed in a similar manner to the unit, by taking the
+  -- inverse of the φ bijection (φr) and mapping 𝒞(R d, R d) to 𝒟(L (R d), d).
+  -- (a natural transformation from L⬝R to Id D).
+  ε := {
+    α := λ d, by exact a.φr (C.id (R d)),
+    -- proof is analogous to η
+    naturality_condition :=
+      begin
+        intros d d' g,
+        apply symm,
+        erw a.naturality_cr,
+        erw C.right_id,
+        rw ← C.left_id (R.map_hom g),
+        erw adjunction.naturality_dr,
+        refl,
+      end,
+  },
+  -- All that is left to do is prove the triangle identities.
+  id_L :=
+    begin
+      intro c,
+      simp,
+      erw a.naturality_cr,
+      erw C.right_id,
+      apply a.retr,
+    end,
+  id_R := 
+    begin
+      intro d,
+      simp,
+      erw a.naturality_d,
+      erw D.left_id,
+      apply a.sect,
+    end,
+}
+
 
 end category_theory
