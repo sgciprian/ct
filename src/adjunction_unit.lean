f0408f69f6b5773d550074ac0a336994f3cfcfa3
Ciprian Stanciu <g.c.stanciu@student.tudelft.nl>
2023-05-30 12:04:13 +0200

    added unit to homset adjunction conversion

--- src/adjunction_unit.lean
+++ src/adjunction_unit.lean
@@ -59,7 +59,7 @@ def adjunction_hom_to_unit {C D} {L : functor C D} {R} (a : adjunction L R)
   -- (a natural transformation from L⬝R to Id D).
   ε := {
     α := λ d, by exact a.φr (C.id (R d)),
-    -- proof is analogous to η
+    -- proof is analogous (also dual) to η
     naturality_condition :=
       begin
         intros d d' g,
@@ -90,5 +90,73 @@ def adjunction_hom_to_unit {C D} {L : functor C D} {R} (a : adjunction L R)
     end,
 }
 
+-- Mac Lane pg. 82, Theorem 1 (6), (7)
+def adjunction_unit_to_hom {C D} {L : functor C D} {R} (a : adjunction_unit L R) : adjunction L R :=
+{
+  -- φ(k)  = R k ∘ η c
+  φ  := λ {c d} (k : D.hom (L c) d), C.compose (R.map_hom k) (a.η.α c),
+  -- φr(h) = ε d ∘ L h
+  φr := λ {c d} (h : C.hom c (R d)), D.compose (a.ε.α d) (L.map_hom h),
+  sect := λ {c d} (h : C.hom c (R d)),
+    begin
+      simp,
+      -- ⊢ R (ε d ∘ L h) ∘ η c = h
+      rw R.comp (L.map_hom h) (a.ε.α d), -- un-distribute R
+      -- ⊢ (R ε d ∘ R L h) ∘ η c = h
+      rw ← C.assoc,                      -- change parantheses
+      -- ⊢ R ε d ∘ (R L h ∘ η c) = h
+      erw a.η.naturality_condition,      -- using η naturality
+      -- ⊢ R ε d ∘ (η R d ∘ h) = h
+      erw C.assoc,                       -- change parantheses again for Lean
+      -- ⊢ (R ε d ∘ η R d) ∘ h = h
+      erw a.id_R,
+      -- ⊢ h = h - well, not actually this, but equivalent to it
+      erw C.right_id,
+      refl,
+    end,
+  retr := λ {c d} (k : D.hom (L c) d),
+      begin
+      simp,
+      -- analogous (dual!!) to section
+      rw L.comp,
+      rw D.assoc,
+      erw ← a.ε.naturality_condition,
+      erw ← D.assoc,
+      erw a.id_L,
+      erw D.left_id, 
+      refl,
+      end,
+  naturality_c :=
+    begin
+      intros,
+      rw ← C.assoc,
+      erw ← a.η.naturality_condition,
+      rw C.assoc,
+      erw ← R.comp,
+    end,
+  naturality_d :=
+    begin
+      intros,
+      rw C.assoc,
+      rw R.comp,
+    end,
+  -- dual to naturality_d
+  naturality_cr :=
+    begin
+      intros,
+      rw ← D.assoc,
+      rw L.comp,
+    end,
+  -- dual to naturality_c
+  naturality_dr :=
+    begin
+      intros,
+      rw D.assoc,
+      erw a.ε.naturality_condition,
+      rw ← D.assoc,
+      erw L.comp,
+      refl,
+    end,
+}
 
 end category_theory
