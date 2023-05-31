b5c64c65cf66af389bb3e8a83a8cd1d27d3bb7c1
Csanád Farkas <farkas.csacsa@gmail.com>
2023-05-31 16:53:25 +0200

    Restructure category and add proof for double square

---
+++ src/category/theorems.lean
@@ -0,0 +1,28 @@
+import category.category
+
+namespace category_theory
+
+  theorem double_square 
+    {C : category}
+    {X Y Z P Q R : C.C₀}
+    {a : C.hom X Y}
+    {b : C.hom Y Z}
+    {c : C.hom P Q}
+    {d : C.hom Q R}
+    {e : C.hom X P}
+    {f : C.hom Y Q}
+    {g : C.hom Z R}
+    :
+    (C.compose g b = C.compose d f) ∧
+    (C.compose f a = C.compose c e) →
+    (C.compose (C.compose g b) a) = (C.compose d (C.compose c e))
+    :=
+    begin
+      intros sq,
+      cases sq with sq1 sq2,
+      rw sq1,
+      rw ←sq2,
+      rw C.assoc,
+    end
+
+end category_theory
