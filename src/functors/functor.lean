a62a8d0926c86c06b37db06688a3028862887c85
Csanád Farkas <farkas.csacsa@gmail.com>
2023-05-31 16:54:21 +0200

    Add composition to natural transformation

--- src/functors/functor.lean
+++ src/functors/functor.lean
@@ -8,20 +8,23 @@ structure functor (C D : category) :=
   (id : ∀ (X : C), map_hom (C.id X) = D.id (map_obj X))
   (comp : ∀ {X Y Z : C} (f : C.hom X Y) (g : C.hom Y Z), map_hom (C.compose g f) = D.compose (map_hom g) (map_hom f))
 
--- Coercion from functor to function.
--- This only works for the object mapping (map_obj!).
--- For morphism mapping use functor_map defined below.
-instance coe_functor_obj {C D : category} : has_coe_to_fun (functor C D) (λ f, C → D) :=
-{
-  coe := λ f, f.map_obj
-}
+  -- notation
+  infixr `=>`:80 := functor
 
-def functor_map {C D : category} (F: functor C D) {X Y : C} (f : C.hom X Y) :  D.hom (F.map_obj X) (F.map_obj Y) := F.map_hom f
+  -- Coercion from functor to function.
+  -- This only works for the object mapping (map_obj!).
+  -- For morphism mapping use functor_map defined below.
+  instance coe_functor_obj {C D : category} : has_coe_to_fun (functor C D) (λ f, C → D) :=
+  {
+    coe := λ f, f.map_obj
+  }
 
-def is_full {C D : category} (F: functor C D) :=
-  ∀ {X Y : C} (g : D.hom (F.map_obj X) (F.map_obj Y)), ∃ (f : C.hom X Y), (F.map_hom f) = g
+  def functor_map {C D : category} (F: functor C D) {X Y : C} (f : C.hom X Y) :  D.hom (F.map_obj X) (F.map_obj Y) := F.map_hom f
 
-def is_faithful {C D : category} (F: functor C D) :=
-  ∀ {X Y : C} {f g : C.hom X Y} (h : functor_map F f = functor_map F g), f = g
+  def is_full {C D : category} (F: functor C D) :=
+    ∀ {X Y : C} (g : D.hom (F.map_obj X) (F.map_obj Y)), ∃ (f : C.hom X Y), (F.map_hom f) = g
+
+  def is_faithful {C D : category} (F: functor C D) :=
+    ∀ {X Y : C} {f g : C.hom X Y} (h : functor_map F f = functor_map F g), f = g
 
 end category_theory
--- src/natural_transformation.lean
+++ src/natural_transformation.lean
@@ -10,4 +10,37 @@ structure natural_transformation {C D : category} (F G : functor C D) :=
     D.compose (α Y) (F.map_hom f)
   )
 
+  -- notation
+  infixr `==>`:90 := natural_transformation
+
+  def bimap {C D E : category} {F F' : D => E} {G G' : C => D} (α : F ==> F') (β : G ==> G')
+    : (F ⬝ G) ==> (F' ⬝ G') := sorry
+
+  infixr (name := bimap) `×`:95 := bimap
+
+  def nt_composition
+    {C D : category}
+    {F G H : C => D}
+    (τ₁ : G ==> H) (τ₂ : F ==> G) : F ==> H :=
+    {
+      α := begin
+        intros,
+        let f := τ₁.α X,
+        let g := τ₂.α X,
+        let h := D.compose f g,
+        exact h,
+      end,
+      naturality_condition := begin
+        intros,
+        let a := τ₁.naturality_condition f,
+        let b := τ₂.naturality_condition f,
+        rw D.assoc,
+        rw a,
+        rw ←D.assoc,
+        rw b,
+        rw D.assoc,
+      end,
+    }
+
+
 end category_theory
