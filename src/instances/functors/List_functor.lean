c7debbf555776a38b5802f194c18a3f391d4545f
Rado <R.A.Todorov@student.tudelft.nl>
2023-06-01 16:11:58 +0200

    Add list and maybe functors

---
+++ src/instances/functors/List_functor.lean
@@ -0,0 +1,58 @@
+import instances.Set_category
+import functors.functor
+
+open category_theory
+
+inductive List (α: Type) : Type
+  | nil : List
+  | cons (head: α) (tail: List) : List
+
+def ListF : functor Set Set :=
+{
+  map_obj := λ A, List A,
+  map_hom :=
+  begin
+    intros α β f as,
+    induction as,
+    case List.nil {
+      exact List.nil
+    },
+    case List.cons : x xs ih {
+      exact List.cons (f x) ih
+    }
+  end,
+  comp :=
+  begin
+    intros _ _ _ f g,
+    simp,
+    apply funext,
+    intro xs,
+    induction xs,
+    case List.nil {
+      simp,
+      refl
+    },
+    case List.cons : x xs ih {
+      simp,
+      rw ih,
+      refl
+    }
+  end,
+  id :=
+  begin
+    intros _,
+    simp,
+    apply funext,
+    intro xs,
+    induction xs,
+    case List.nil {
+      simp,
+      refl
+    },
+    case List.cons : x xs ih {
+      simp,
+      rw ih,
+      refl
+    }
+  end
+}
---
+++ src/instances/functors/Maybe_functor.lean
@@ -0,0 +1,56 @@
+import instances.Set_category
+import functors.functor
+
+open category_theory
+
+inductive Maybe (α : Type)
+  | none : Maybe
+  | some : α → Maybe
+
+def MaybeF : functor Set Set :=
+{
+  map_obj := λ A, Maybe A,
+  map_hom :=
+  begin
+    intros α β f as,
+    induction as,
+    case Maybe.none {
+      exact Maybe.none
+    },
+    case Maybe.some : x {
+      exact Maybe.some (f x)
+    }
+  end,
+  comp :=
+  begin
+    intros _ _ _ f g,
+    simp,
+    apply funext,
+    intro xs,
+    induction xs,
+    case Maybe.none {
+      simp,
+      refl
+    },
+    case Maybe.some : x {
+      simp,
+      refl
+    }
+  end,
+  id :=
+  begin
+    intros _,
+    simp,
+    apply funext,
+    intro xs,
+    induction xs,
+    case Maybe.none {
+      simp,
+      refl
+    },
+    case Maybe.some : x {
+      simp,
+      refl
+    }
+  end
+}
