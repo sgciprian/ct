9f0da2ec2977eaf65e25edbfe2aaa8118a3bb148
Csanád Farkas <farkas.csacsa@gmail.com>
2023-06-01 11:40:45 +0200

    Add the functor composition associativity law

--- src/functors/composition.lean
+++ src/functors/composition.lean
@@ -2,25 +2,28 @@ import functors.functor
 
 namespace category_theory
 
--- Creates the composition of two functors.
-def composition_functor {C D E : category} (G : functor D E) (F : functor C D) : functor C E :=
-{
-  map_obj := λ X : C, G.map_obj (F.map_obj X),
-  map_hom := λ X Y : C, λ f, G.map_hom (F.map_hom f),
-  id :=
-    begin
-      intro,
-      rw F.id,
-      rw G.id,
-    end,
-  comp :=
-    begin
-      intros,
-      rw F.comp,
-      rw G.comp,
-    end,
-}
--- notation
-infixr `⬝`:90 := composition_functor
+  -- Creates the composition of two functors.
+  def composition_functor {C D E : category} (G : functor D E) (F : functor C D) : functor C E :=
+  {
+    map_obj := λ X : C, G.map_obj (F.map_obj X),
+    map_hom := λ X Y : C, λ f, G.map_hom (F.map_hom f),
+    id :=
+      begin
+        intro,
+        rw F.id,
+        rw G.id,
+      end,
+    comp :=
+      begin
+        intros,
+        rw F.comp,
+        rw G.comp,
+      end,
+  }
+  -- notation
+  infixr `⬝`:90 := composition_functor
+
+  theorem functor_comp_assoc {C D E F : category} (H : functor E F) (G : functor D E) (F : functor C D) :
+    (H ⬝ G) ⬝ F = H ⬝ (G ⬝ F) := rfl
 
 end category_theory
