e0c45d652b3d3bcaf251687b3e5b551d31473c27
Pedro Brandão <phbrandao1206@gmial.com>
2023-06-22 18:56:00 +0200

    added category of coalgebras and definition of final coalgebra

--- src/coalgebras/coalgebra.lean
+++ src/coalgebras/coalgebra.lean
@@ -1,21 +1,15 @@
-import ..functors.functor
+import ..functors.functor ..universal_properties
 
 universe w
 
 namespace category_theory
 
--- structure coalgebra (C: category) [F : functor C C] :=
--- (A : C.C₀)
--- (ϕ : C.hom A (F.map_obj A))
-
 structure coalgebra {C: category} (F : functor C C) :=
-(A : C.C₀)
-(ϕ : C.hom A (F.map_obj A))
+(object : C.C₀)
+(morphism : C.hom object (F.map_obj object))
 
--- structure coalgebra (C: category) :=
--- (A : C.C₀)
--- (F : functor C C)
--- (ϕ : C.hom A (F.map_obj A))
-
+structure f_coalgebra_homomorphism {C : category} {F : functor C C} (A B : coalgebra F) :=
+  (morphism : C.hom A.object B.object)
+  (proof : C.compose B.morphism morphism = C.compose (F.map_hom morphism) A.morphism) 
 
 end category_theory
---
+++ src/coalgebras/final_coalgebra.lean
@@ -0,0 +1,14 @@
+import .coalgebras_category
+
+namespace category_theory
+
+structure final_coalgebra {C : category} (F : functor C C) :=
+  (obj : coalgebra F)
+  (anamorphism : Π (A : coalgebra F), (coalgebra_category F).hom A obj)
+  (unique :  ∀ {A : (coalgebra_category F).C₀} (f : (coalgebra_category F).hom A obj), f = anamorphism A)
+
+-- theorem id_property {C : category} (F : functor C C) (A : final_coalgebra F) : A.obj.id
+
+-- theorem theorem207 {C : category} (F : functor C C) (A B : coalgebra F) (v : final_coalgebra F) : C.compose B.morphism (f_coalgebra_homomorphism A B) :=
+
+end category_theory
