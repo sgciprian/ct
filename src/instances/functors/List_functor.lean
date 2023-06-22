e3da3331ea0df82bf6933d4eef655f1300226517
Csanád Farkas <farkas.csacsa@gmail.com>
2023-06-22 17:39:48 +0200

    Add list monad

--- src/instances/functors/List_functor.lean
+++ src/instances/functors/List_functor.lean
@@ -7,27 +7,39 @@ inductive List (α: Type) : Type
   | nil : List
   | cons (head: α) (tail: List) : List
 
+notation `[]` := List.nil
+notation (name := List_construction) h::t := List.cons h t
+
+def List.merge {α : Type} : List α → List α → List α
+  | List.nil ys := ys
+  | (List.cons x xs) ys := List.cons x (List.merge xs ys)
+
+notation (name := list_merge) xs `++` ys := List.merge xs ys
+
+def List.fmap {α β: Type} (f: α → β) : List α → List β
+  | List.nil := List.nil
+  | (List.cons head tail) := List.cons (f head) (List.fmap tail)
 
 -- Example of the List functor 
 -- as known from Functional Programming Languages
-def ListFunctor : functor Set Set :=
+def List.functor : functor Set Set :=
 {
   -- Objects are mapped to Lists 
   map_obj := λ A, List A,
   -- Morphisms are mapped using induction over the input List
   -- In the base case we return Nil, and when mapping Cons (head, tail)
   -- we apply the given function to the head element and recursively continue with the tail. 
-  map_hom :=
-  begin
-    intros α β f as,
-    induction as,
-    case List.nil {
-      exact List.nil
-    },
-    case List.cons : x xs ih {
-      exact List.cons (f x) ih
-    }
-  end,
+  map_hom := λ _ _ f, List.fmap f,
+  /- begin -/
+  /-   intros α β f as, -/
+  /-   induction as, -/
+  /-   case List.nil { -/
+  /-     exact List.nil -/
+  /-   }, -/
+  /-   case List.cons : x xs ih { -/
+  /-     exact List.cons (f x) (ih) -/
+  /-   } -/
+  /- end, -/
   -- Induction is used to prove that composition is preserved
   -- Here, the base case still returns Nil.
   -- Given Cons(head, tail) lean is smart and understands that 
@@ -36,16 +48,17 @@ def ListFunctor : functor Set Set :=
   comp :=
   begin
     intros _ _ _ f g,
-    simp,
+    -- simp,
     apply funext,
     intro xs,
     induction xs,
     case List.nil {
-      simp,
+      -- simp,
       refl,
     },
     case List.cons : x xs ih {
-      simp,
+      -- simp,
+      rw List.fmap,
       rw ih,
       refl,
     }
@@ -56,16 +69,17 @@ def ListFunctor : functor Set Set :=
   id :=
   begin
     intros _,
-    simp,
+    -- simp,
     apply funext,
     intro xs,
     induction xs,
     case List.nil {
-      simp,
+      -- simp,
       refl,
     },
     case List.cons : x xs ih {
-      simp,
+      -- simp,
+      rw List.fmap,
       rw ih,
       refl,
     }
